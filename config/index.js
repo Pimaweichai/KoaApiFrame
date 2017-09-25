581bd5816ca53d8ef9fc662ce35076b40ef110c8
pimaweichai <1078678909@qq.com>
2017-09-25 14:55:44 +0800

    update

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
-const env = process.env.NODE_ENV === 'production'? 'production':'development';
+const env = process.env.NODE_ENV || 'development';
 const config = {
     development: require('./dev.config.js'),
     production:require('./pro.config.js')
